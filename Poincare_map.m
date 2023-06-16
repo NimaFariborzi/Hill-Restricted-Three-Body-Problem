31b3d2c88f57bee63dd45d5cebb0c3cb38250ac1
Christopher Lin <115333635+chris28791@users.noreply.github.com>
2023-06-15 17:05:06 -0700

    Update Poincare_map.m

--- Poincare_map.m
+++ Poincare_map.m
@@ -2,20 +2,17 @@ close all
 clearvars
 clc
 
-%Initial conditions(all planar)
 
-%X01=[(1/3)^(1/3)-0.03; 1.e-11;0;0]; %IC around L2
-
-xi1 = .28350;
+%periodic IC
+xi1 = .28350; 
 J1 = 4.49999;
 eta_dot1 = sqrt(3*xi1^2 + 2/xi1 - J1);
-
-xi2 = .56291;
-J2 = 4.300000;
-eta_dot2 = sqrt(3*xi2^2 + 2/xi2 - J2);
-
 X01=[xi1;0;0;eta_dot1];
-X02=[xi2;0;0;eta_dot2];
+
+%IC around L2 with slight perturbation
+X02=[(1/3)^(1/3)-0.03;1.e-11;0.0025;0.0025];
+
+
 
 options = odeset('AbsTol',1e-12,'RelTol',1e-12,'Events',@xcross_event); 
 tspan = [0,1500]; 
@@ -28,36 +25,27 @@ vx1=Xevent1(:,3);
 x2=Xevent2(:,1);
 vx2=Xevent2(:,3);
 
+
+
 figure(1)
 plot(x1,vx1,'k.',x1,-vx1,'k.','MarkerSize',15)
-title('Poincare Surface of Section(y=0,ydot<0)')
+title('Poincare Surface of Section(for J=4.49999 and x0=.2835)')
 xlabel('x')
 ylabel('xdot')
 grid on;
 
 
 figure(2)
-plot(x2,vx2,'r.',x2,-vx2,'r.','MarkerSize',15)
-title('Poincare Surface of Section(y=0,ydot<0)')
+plot(x2,vx2,'r.','MarkerSize',10)
+title('Poincare Surface of Section near L2 with perturbation')
 xlabel('x')
 ylabel('xdot')
 grid on;
 
+
 %% functions
 
-function sys_of_1st_ordereqns=eom_hR3bp_3d(X)
-x=X(1);
-y=X(2);
-z=X(3);
-dxdt=X(4); %vx
-dydt=X(5); %vy
-dzdt=X(6); %vz
-r=(x^2+y^2+z^2)^0.5;
-dvxdt=2*dydt-(x/(r^3))+3*x; %ax
-dvydt=-2*dxdt-(y/(r^3)); %ay
-dvzdt=-(z/(r^3))-z; %az
-sys_of_1st_ordereqns=[dxdt;dydt;dzdt;dvxdt;dvydt;dvzdt];
-end
+
 
 function sys_of_1st_ordereqns=eom_hR3bp_2d(X)
 x=X(1);
